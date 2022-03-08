412a139042add383ff6a7585bcbf76c8decaf8b3
iQuickJP <josebarthio@gmail.com>
2022-03-08 20:22:01 +0100

    spectral sprite

--- simplephasergame-1-main/src/objetos_recogibles/pasivos/spectral.js
+++ simplephasergame-1-main/src/objetos_recogibles/pasivos/spectral.js
@@ -3,7 +3,7 @@ import PowerUp from "../powerUp.js";
 export default class Spectral extends PowerUp{
 
     constructor(scene,player,x,y){
-        super(scene,player,x,y,'');
+        super(scene,player,x,y,'flecha');
     }
     
     givePower(){
