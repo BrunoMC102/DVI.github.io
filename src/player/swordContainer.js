0f434b14daab15b791ee71c614d46cc86273babc
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2022-03-19 20:05:47 +0100

    cambiado movimiento espada

--- src/player/swordContainer.js
+++ src/player/swordContainer.js
@@ -10,17 +10,18 @@ export default class SwordContainer extends Phaser.GameObjects.Container{
         this.setVisible(true);
         this.interiorContainer = new Phaser.GameObjects.Container(scene, 10, 0);
         this.add(this.interiorContainer);
-        this.sword = new Sword(scene, 50, 0, player);
+        this.sword = new Sword(scene, 40, 0, player);
         this.interiorContainer.add(this.sword);
-        this.interiorContainer.rotation = -Math.PI/2;
+        this.interiorContainer.rotation = -Math.PI/2-0.7;
         this.noAttackrotation = this.interiorContainer.rotation;
-        this.swordNoAttackRotation = -Math.PI/2;
-        this.sword.rotation = -Math.PI/2;
+        this.swordNoAttackRotation = -Math.PI/2-0.7;
+        this.sword.rotation = -Math.PI/2-0.7;
         this.player = player;
         this.espera = false;
-        this.timeAttack = 200;
-        this.AttackSpeed = 300;
+        this.timeAttack = 300;
+        this.AttackSpeed = 350;
         this.positioning = false;
+        this.rotacion = Math.PI + 1.4;
     }
 
 
@@ -34,13 +35,22 @@ export default class SwordContainer extends Phaser.GameObjects.Container{
             }
         }
         else {
-
-            if(this.positioning) this.interiorContainer.rotation -= dt*Math.PI/this.timeAttack;  
-            else this.interiorContainer.rotation += dt*Math.PI/this.timeAttack;  
+            if(this.fase == 0){
+                if(this.positioning) this.interiorContainer.rotation -= dt*this.rotacion*5*0.2/this.timeAttack;  
+                else this.interiorContainer.rotation += dt*this.rotacion*2.5*0.05/this.timeAttack;  
+            }
+            else if(this.fase == 1){
+                if(this.positioning) this.interiorContainer.rotation -= dt*this.rotacion*5*0.5/this.timeAttack;  
+                else this.interiorContainer.rotation += dt*this.rotacion*5*0.6/this.timeAttack; 
+            }
+            else{
+                if(this.positioning) this.interiorContainer.rotation -= dt*this.rotacion*(5/3)*0.3/this.timeAttack;  
+                else this.interiorContainer.rotation += dt*this.rotacion*2.5*0.25/this.timeAttack; 
+            }
 
             if(this.fase == 0 || this.fase == 2)
-                if(this.positioning) this.sword.rotation -= dt*2*3/this.timeAttack;
-                else this.sword.rotation += dt*2*3/this.timeAttack;
+                if(this.positioning) this.sword.rotation -= dt*(5/4)*(Math.PI+1.4)/this.timeAttack;
+                else this.sword.rotation += dt*(5/4)*(Math.PI+1.4)/this.timeAttack;
         }
     }
 
@@ -50,17 +60,17 @@ export default class SwordContainer extends Phaser.GameObjects.Container{
         this.attacking = true;
         this.espera = true;
         this.fase = 0;
-        this.scene.time.delayedCall(this.timeAttack/4, ()=>{this.fase = 1});
-        this.scene.time.delayedCall(this.timeAttack*3/4, ()=>{this.fase = 2});
+        this.scene.time.delayedCall(this.timeAttack*1/5, ()=>{this.fase = 1});
+        this.scene.time.delayedCall(this.timeAttack*2/5, ()=>{this.fase = 2});
         this.scene.time.delayedCall(this.timeAttack, ()=> {
             this.positioning = !this.positioning
             if(this.positioning){ 
-                this.noAttackrotation = Math.PI/2;
-                this.swordNoAttackRotation = Math.PI/2;
+                this.noAttackrotation = Math.PI/2+0.7;
+                this.swordNoAttackRotation = Math.PI/2+0.7;
             }
             else{
-                this.noAttackrotation = -Math.PI/2;
-                this.swordNoAttackRotation = -Math.PI/2;
+                this.noAttackrotation = -Math.PI/2-0.7;
+                this.swordNoAttackRotation = -Math.PI/2-0.7;
             }
 
             this.attacking = false; 
