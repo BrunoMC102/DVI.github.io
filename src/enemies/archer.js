150385634458e5d1a79c2b474e22ce8e0a212f68
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2022-03-29 02:43:01 +0200

    añadido inmunidad arquero

--- src/enemies/archer.js
+++ src/enemies/archer.js
@@ -14,45 +14,51 @@ export default class Archer extends ShootingEnemyParent {
         this.fireDirection = new Phaser.Math.Vector2(0, 1);
         this.arrows = 5;
         this.circun = 0.2;
-        this.shootTime = 5;
+        this.shootTime = 3;
         this.charging = false;
         this.cont = 1;
         this.dashing = false;
         this.dashDelay = false;
         this.escaping = false;
         this.preCharging = false;
-        this.v = 200;
+        this.v = 250;
         //this.scale = 2;
         this.changeEscapeVector();
         this.changemoveVector();
         this.body.offset.x = -8;
         this.body.offset.y = 54;
         this.body.setSize(this.body.width * 1.4, this.body.height * 1.7);
+        this.dispOffset = 0;
+        this.atrapadoVector = new Phaser.Math.Vector2(this.x, this.y);
+        this.atrapado = false;
+        this.atrapadoCont = 0;
     }
 
 
     creador() {
-        return new Basic_projectile(this.scene, this.centerX(), this.centerY(), 'flecha', this.fireDirection.x * this.Pv, this.fireDirection.y * this.Pv, 10, this.projectileDamage);
+        return new Basic_projectile(this.scene, this.body.center.x + this.dispOffset, this.body.center.y, 'flecha', this.fireDirection.x * this.Pv, this.fireDirection.y * this.Pv, 10, this.projectileDamage);
     }
 
 
     attack(d, dt) {
-        if (!this.dashing) {
-            if (!this.escaping) {
-                if (this.cont === 0) {
-                    this.fire();
-                }
-                this.cont += dt;
-                if (this.cont >= (this.shootTime - 1) * 1000) {
-                    this.preCharging = true;
-                }
-                if (this.cont >= (this.shootTime - 0.35) * 1000) {
-                    this.preCharging = false;
-                    this.charging = true;
-                }
-                if (this.cont >= this.shootTime * 1000) {
-                    this.cont = 0;
-                    this.scene.time.delayedCall(500, () => {this.charging = false;})
+        if (!this.atrapado) {
+            if (!this.dashing) {
+                if (!this.escaping) {
+                    if (this.cont === 0) {
+                        this.fire();
+                    }
+                    this.cont += dt;
+                    if (this.cont >= (this.shootTime - 1) * 1000) {
+                        this.preCharging = true;
+                    }
+                    if (this.cont >= (this.shootTime - 0.35) * 1000) {
+                        this.preCharging = false;
+                        this.charging = true;
+                    }
+                    if (this.cont >= this.shootTime * 1000) {
+                        this.cont = 0;
+                        this.scene.time.delayedCall(500, () => { this.charging = false; })
+                    }
                 }
             }
         }
@@ -61,37 +67,43 @@ export default class Archer extends ShootingEnemyParent {
 
     moveU(t, dt) {
         let xdir = this.player.x - this.x;
-        if(xdir > 0)
+        if (xdir > 0) {
             this.sprite.flipX = false;
-        else
+            this.dispOffset = 50;
+        }
+        else {
             this.sprite.flipX = true;
-        
+            this.dispOffset = -50;
+        }
 
         
-        if (this.preCharging){
+
+        if (this.preCharging) {
             this.sprite.stop();
-            this.body.setVelocity(0,0);
+            this.body.setVelocity(0, 0);
             return;
         }
         if (this.charging) {
             this.sprite.play('archerfire', true);
-            this.body.setVelocity(0,0);
+            this.body.setVelocity(0, 0);
         }
         else {
-            const dx = new Phaser.Math.Vector2(this.player.x - this.x, this.player.y - this.y);
-            if (dx.length() < 200) {
-                if (!this.dashDelay) {
-                    this.dashing = true;
-                    this.dashDelay = true;
-                    this.scene.time.delayedCall(500, () => {
-                        this.dashing = false;
-                        this.changeEscapeVector();
-                        this.changemoveVector();
-                    })
-                    this.scene.time.delayedCall(4000, () => {
-                        this.dashDelay = false;
-                    })
-                    return;
+            const dx = new Phaser.Math.Vector2(this.player.x - this.centerX(), this.player.y - this.centerY());
+            if(!this.atrapado){
+                if (dx.length() < 200) {
+                    if (!this.dashDelay) {
+                        this.dashing = true;
+                        this.dashDelay = true;
+                        this.scene.time.delayedCall(500, () => {
+                            this.dashing = false;
+                            this.changeEscapeVector();
+                            this.changemoveVector();
+                        })
+                        this.scene.time.delayedCall(4000, () => {
+                            this.dashDelay = false;
+                        })
+                        return;
+                    }
                 }
             }
             if (dx.length() < 350) {
@@ -109,40 +121,109 @@ export default class Archer extends ShootingEnemyParent {
                 }
             }
 
-        
-        if(this.dashing){
-            this.body.setVelocity(this.escapeVector.x*2, this.escapeVector.y*2);
-            this.sprite.play('archerjump', true);
-        }
-        else if (this.escaping) {
-            this.body.setVelocity(this.escapeVector.x, this.escapeVector.y);
-            this.sprite.play('archerMove', true);
-        }
-        else{
-            this.body.setVelocity(this.moveVector.x/2,this.moveVector.y/2);
-            this.sprite.play('archerMove',true);
+            if(this.atrapado){
+                this.sprite.play('archeridle', true);
+                this.body.setVelocity(0,0);
+                this.sprite.tint = 0x00ff00;
+            }
+            else{
+                if (this.dashing) {
+                    this.body.setVelocity(this.escapeVector.x * 2, this.escapeVector.y * 2);
+                    this.sprite.play('archerjump', true);
+                }
+                else if (this.escaping) {
+                    this.body.setVelocity(this.escapeVector.x, this.escapeVector.y);
+                    this.sprite.play('archerMove', true);
+                }
+                else {
+                    this.body.setVelocity(this.moveVector.x / 2, this.moveVector.y / 2);
+                    this.sprite.play('archerMove', true);
+                }
+            }
+            this.checkatrapado(dt);
         }
     }
 
+
+
+    hurt(damage){
+        if(this.atrapado) return;
+        super.hurt(damage);
     }
+    knockback(x,y,p){
+        if(this.atrapado) return;
+        super.knockback(x,y,p);
+    }
+
+    freeze(){
+        if(this.atrapado) return;
+        super.freeze(damage);
+    }
+
+    die() {
+        this.sprite.play('archerDie');
+        this.body.destroy();
+        this.preUpdate = () => { };
+        if (this.cambio != undefined)
+            this.cambio.remove();
+        if (this.escambio != undefined)
+            this.escambio.remove();
+        this.scene.time.delayedCall(1500, () => {
+            this.spawnMana();
+            this.destroy();
+            this.spawnLoot();
+        })
+    }
+
+
+    checkatrapado(dt) {
+
+        if (this.atrapado) {
+            if (!this.escaping){
+                this.atrapado = false;
+                this.sprite.tint = this.origTint;
+                this.atrapadoCont = 0;
+            }
+        }
+        this.actVector = new Phaser.Math.Vector2(this.x, this.y);
+        if (this.actVector.distance(this.atrapadoVector) < 50) {
+            this.atrapadoCont += dt;
+        }
+        else {
+            this.atrapadoCont = 0;
+            this.atrapadoVector = new Phaser.Math.Vector2(this.x, this.y);
+        }
+        if (this.atrapadoCont > 1000) {
+            this.atrapado = true;
+        }
+
+    }
+
     changeEscapeVector() {
         const dx = new Phaser.Math.Vector2(-(this.player.x - this.x), -(this.player.y - this.y));
         let ang = (Math.random() * 2 - 1) * 0.2 * Math.PI;
         dx.normalize().rotate(ang).scale(this.v);
         this.escapeVector = dx;
+        if (this.escambio != undefined)
+            this.escambio.remove();
+        this.escambio = this.scene.time.delayedCall(1000, () => {
+            if (this.dead) return;
+            this.changeEscapeVector();
+        });
     }
-    changemoveVector(){
+    changemoveVector() {
         const dx = new Phaser.Math.Vector2(this.player.x - this.x, this.player.y - this.y);
-        let ang = (Math.random() * (Math.PI*2/4 - Math.PI/4) + Math.PI/4)
+        let ang = (Math.random() * (Math.PI * 2 / 4 - Math.PI / 4) + Math.PI / 4)
         let dir = Math.random();
-        if(dir < 0.5){
+        if (dir < 0.5) {
             ang = -ang;
-        } 
+        }
         dx.normalize().rotate(ang).scale(this.v);
         this.moveVector = dx;
-        if(this.cambio != undefined)
+        if (this.cambio != undefined)
             this.cambio.remove();
-        this.cambio = this.scene.time.delayedCall(2000,()=>{
+        this.cambio = this.scene.time.delayedCall(2000, () => {
+            if (this.dead) return
             this.changemoveVector();
         });
 
@@ -161,11 +242,17 @@ export default class Archer extends ShootingEnemyParent {
             this.boo = 0;
         }
     }
-    isCol(){
-        if(this.dashing)
+    isCol() {
+        if (this.dashing)
             return;
         this.changeEscapeVector();
         this.changemoveVector();
 
+    }
+    centerX(){
+        return this.body.center.x;
+    }
+    centerY(){
+        return this.body.center.y;
     }
 }
