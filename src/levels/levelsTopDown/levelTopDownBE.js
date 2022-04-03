01cb516df981e3b5c196cfc622999414d70d98f4
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2022-04-03 13:57:45 +0200

    correccion en los niveles

--- src/levels/levelsTopDown/levelTopDownBE.js
+++ src/levels/levelsTopDown/levelTopDownBE.js
@@ -14,17 +14,6 @@ export default class LevelTopDownBE extends LevelParent {
   
    
   
-    createOthers(){
-      new Chest(this, this.player, 450, 300);
-      new Coin(this, this.player, 450, 200);
-      new HealthPotion(this, this.player, 600, 200);
-      new Health(this, this.player, 750, 200);
-      new Arrow(this, this.player, 900, 200);
-    }
-  
-    createEnemies(){
-      return [new Minotaur(this, this.player, 500, 500), new Enemy(this, this.player, 700, 500)];
-    }
     
   
     setTileSet() {
